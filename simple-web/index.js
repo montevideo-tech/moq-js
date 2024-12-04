b02f81de12b3bc33132f78347b04d1c37dabd9e6
Joaquin Bartaburu <joaquinb@qualabs.com>
2024-12-04 18:28:21 -0300

    fix: remove spanish comments

--- simple-web/index.js
+++ simple-web/index.js
@@ -1,29 +1,20 @@
-const express = require("express")
-const cors = require("cors")
-const path = require("path")
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
 
-const app = express()
-const PORT = 3000
+const app = express();
+const PORT = 3000;
 
-// Middleware para habilitar CORS
-app.use(cors())
+app.use(cors());
 
-// Middleware para configurar los headers globalmente
 app.use((req, res, next) => {
-	res.setHeader("Cross-Origin-Opener-Policy", "same-origin")
-	res.setHeader("Cross-Origin-Embedder-Policy", "require-corp")
-	next()
-})
+	res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+	res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+	next();
+});
 
-// Servir archivos estáticos desde la carpeta 'public'
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, "public")));
 
-// Ruta de ejemplo
-app.get("/api", (req, res) => {
-	res.json({ message: "Headers configurados correctamente con CORS habilitado" })
-})
-
-// Inicializar servidor
 app.listen(PORT, () => {
-	console.log(`Servidor ejecutándose en http://localhost:${PORT}`)
-})
+	console.log(`Server running in http://localhost:${PORT}`);
+});
