a8ed3c25738fb69d2d6ecc04c9e685ea2772560f
santipuppoQualabs <santiagop@qualabs.com>
2024-12-09 20:24:40 -0300

    Feat #10: Added config to run on https as in web workspace

--- web_component_demo/vite.config.ts
+++ web_component_demo/vite.config.ts
@@ -1,13 +1,23 @@
 import { defineConfig } from "vite";
+import mkcert from "vite-plugin-mkcert";
+import crossOriginIsolation from "vite-plugin-cross-origin-isolation";
 
 export default defineConfig({
 	root: "./public",
-	server: {
-		port: 3000,
-	},
 	build: {
 		outDir: "../dist",
 	},
+	server: {
+		port: 3000,
+		// HTTPS is required for SharedArrayBuffer
+		https: true,
+	},
+	plugins: [
+		// Generates a self-signed certificate using mkcert
+		mkcert(),
+		// Required for SharedArrayBuffer
+		crossOriginIsolation(),
+	],
 	resolve: {
 		alias: {
 			"@": "/src",
