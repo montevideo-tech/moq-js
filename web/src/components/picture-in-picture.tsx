d193d5dafd17a1937ebd004cf1b635238c8e56c3
Chris Triantafilis <christophertriantafilis@gmail.com>
2024-12-14 15:28:16 -0500

    Add global.d.ts file

--- web/src/components/picture-in-picture.tsx
+++ web/src/components/picture-in-picture.tsx
@@ -1,24 +1,6 @@
 import { onCleanup } from "solid-js"
 import { pipState, setPipState } from "src/store/state"
 
-interface DocumentPictureInPicture {
-	requestWindow: (options: { width: number; height: number }) => Promise<WindowWithPiP>
-	window: Window | null
-}
-
-interface WindowWithPiP extends Window {
-	document: Document
-	close: () => void
-	addEventListener: (type: string, listener: EventListener) => void
-	removeEventListener: (type: string, listener: EventListener) => void
-}
-
-interface Window {
-	documentPictureInPicture?: DocumentPictureInPicture
-}
-
-declare let documentPictureInPicture: DocumentPictureInPicture
-
 export const PictureInPictureButton = () => {
 	let videoCanvasElement: HTMLElement | null
 	let pipWindow: WindowWithPiP | null
