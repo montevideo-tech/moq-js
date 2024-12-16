94144352cafd43d2fbd736ce7fb1d522e1aad2d1
Manuel Barrabino <manuel.barrabino@qualabs.com>
2024-12-16 12:14:11 -0300

    refactor: event manager as const

--- lib/playback/event_manager.ts
+++ lib/playback/event_manager.ts
@@ -41,34 +41,23 @@ export default class EventManager {
 
 type EventManagerMap = { [id: string]: EventManager }
 
-// eslint-disable-next-line @typescript-eslint/no-extraneous-class
-export class EventManagerFactory {
-	private static eventManagers: EventManagerMap = {}
+export const EventManagerFactory = {
+	eventManagers: {} as EventManagerMap,
+	canvasIdMap: new WeakMap<OffscreenCanvas, string>(),
+	canvasIdCounter: 1,
 
-	static getEventManager(canvas: OffscreenCanvas): EventManager {
+	getEventManager(canvas: OffscreenCanvas): EventManager {
 		const key = this.getUniqueCanvasKey(canvas)
-		console.error("Pidieron la lista de eventos")
-		console.error("Key: ", key)
-		console.error("Antes: ", this.eventManagers)
 		if (!this.eventManagers[key]) {
 			this.eventManagers[key] = new EventManager()
-			console.error("Despues: ", this.eventManagers)
 		}
 		return this.eventManagers[key]
-	}
+	},
 
-	private static canvasIdMap: WeakMap<OffscreenCanvas, string>
-	private static canvasIdCounter = 1
-
-	static getUniqueCanvasKey(canvas: OffscreenCanvas): string {
-		console.error("week map: ", this.canvasIdMap)
-		if (!this.canvasIdMap) {
-			this.canvasIdMap = new WeakMap<OffscreenCanvas, string>()
-		}
+	getUniqueCanvasKey(canvas: OffscreenCanvas): string {
 		if (!this.canvasIdMap.has(canvas)) {
-			console.error("Se creo una nueva key")
 			this.canvasIdMap.set(canvas, `canvas-${this.canvasIdCounter++}`)
 		}
 		return this.canvasIdMap.get(canvas)!
-	}
+	},
 }
